ac9f2e5631eb3bfdfbf1e2d1abe93d1a1fe9a76a
Lucas <lucas.silva1037@gmail.com>
2023-09-03 18:52:14 -0300

    up

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
-import { configDefaults, defineConfig, mergeConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
 import vitestConfig from "./vite.config";
 
 
 export default mergeConfig(vitestConfig, defineConfig({
     test: {
-        exclude: [...configDefaults.exclude, "src/__tests__/unit/**"],
+        include: ["src/__tests__/e2e/**"],
+        exclude: ["src/__tests__/unit/**"],
         //environmentMatchGlobs: [['src/__tests__/e2e/**', 'prisma']] // problemático
     }
 })
